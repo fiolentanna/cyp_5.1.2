dd12c0e5abc707a6827c1e02b21219f02f419f37
fiolentanna <tigranka@gmail.com>
2024-10-26 15:52:36 +0300

    5.2.1 created dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "suvvpd",
+  // ...rest of the Cypress project config
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
